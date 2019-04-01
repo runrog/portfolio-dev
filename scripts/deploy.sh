1a6eab791ecfcb31fdea5895738f2a4acfee35ef
runrog <camarorog@yahoo.com>
2019-03-31 19:40:29 -0500

    updating deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,15 @@ set -e
 # run build
 gulp build-dist
 
-# deploy to github pages
-git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist origin gh-pages
+
+# save submodule changes
+cd dist
+git add --all
+git commit -m "update dist"
+git push origin gh-pages
+cd -
+git add dist
+git commit -m "updating master dist"
+git push origin master
+# make PR master <- gh-pages
